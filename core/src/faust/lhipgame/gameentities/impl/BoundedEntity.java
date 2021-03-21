3cbe7594dfacf334aeac02b8b18cd82334aa7244
Faust <jacopo.buttiglieri@gmail.com>
2021-03-21 01:22:34 +0100

    added bounded enemy draft

---
+++ core/src/faust/lhipgame/gameentities/impl/BoundedEntity.java
@@ -0,0 +1,79 @@
+package faust.lhipgame.gameentities.impl;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import faust.lhipgame.gameentities.AnimatedEntity;
+import faust.lhipgame.gameentities.enums.Direction;
+import faust.lhipgame.gameentities.enums.GameBehavior;
+
+import java.util.Arrays;
+
+/**
+ *
+ * Strix enemy Entity class
+ *
+ * @author Jacopo "Faust" Buttiglieri
+ */
+public class StrixEntity extends AnimatedEntity {
+
+    private Texture shadow;
+
+    public StrixEntity(AssetManager assetManager) {
+        super(assetManager.get("sprites/strix_sheet.png"));
+        shadow = assetManager.get("sprites/shadow.png");
+    }
+
+    @Override
+    protected void initAnimations() {
+
+        TextureRegion[] allFrames = getFramesFromTexture();
+
+        TextureRegion[] idleFramesDown = Arrays.copyOfRange(allFrames, 0, getTextureColumns());
+        TextureRegion[] idleFramesLeft = Arrays.copyOfRange(allFrames, getTextureColumns(), getTextureColumns()*2);
+        TextureRegion[] idleFramesUp = Arrays.copyOfRange(allFrames, getTextureColumns()*2, getTextureColumns() * 3);
+        TextureRegion[] idleFramesRight = Arrays.copyOfRange(allFrames, getTextureColumns()*3, getTextureColumns() * 4);
+        TextureRegion[] walkFramesDown = Arrays.copyOfRange(allFrames, getTextureColumns() * 4, getTextureColumns() * 5);
+        TextureRegion[] walkFramesLeft = Arrays.copyOfRange(allFrames, getTextureColumns() * 5, getTextureColumns() * 6);
+        TextureRegion[] walkFramesUp = Arrays.copyOfRange(allFrames, getTextureColumns() * 6, getTextureColumns() * 7);
+        TextureRegion[] walkFramesRight = Arrays.copyOfRange(allFrames, getTextureColumns() * 7, getTextureColumns() * 8);
+        TextureRegion[] attachedFrames = Arrays.copyOfRange(allFrames, getTextureColumns() * 8, getTextureColumns() * 9);
+
+        // Initialize the Idle Animation with the frame interval and array of frames
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, idleFramesDown), GameBehavior.IDLE, Direction.DOWN);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, idleFramesLeft), GameBehavior.IDLE, Direction.LEFT);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, idleFramesUp), GameBehavior.IDLE, Direction.UP);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, idleFramesRight), GameBehavior.IDLE, Direction.RIGHT);
+
+        // Initialize the Walk Animation with the frame interval and array of frames
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesDown), GameBehavior.WALK, Direction.DOWN);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesLeft), GameBehavior.WALK, Direction.LEFT);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesUp), GameBehavior.WALK, Direction.UP);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesRight), GameBehavior.WALK, Direction.RIGHT);
+
+        addAnimation(new Animation<>(FRAME_DURATION, attachedFrames), GameBehavior.ATTACK);
+
+        // Initialize the Hurt Animation with the frame interval and array of frames
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesDown), GameBehavior.HURT, Direction.DOWN);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesLeft), GameBehavior.HURT, Direction.LEFT);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesUp), GameBehavior.HURT, Direction.UP);
+        addAnimationForDirection(new Animation<>(FRAME_DURATION, walkFramesRight), GameBehavior.HURT, Direction.RIGHT);
+
+    }
+
+    @Override
+    protected int getTextureColumns() {
+        return 4;
+    }
+
+    @Override
+    protected int getTextureRows() {
+        return 9;
+    }
+
+    public Texture getShadowTexture() {
+        return shadow;
+    }
+
+}
---
+++ core/src/faust/lhipgame/instances/impl/BoundedInstance.java
@@ -0,0 +1,261 @@
+package faust.lhipgame.instances.impl;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.Timer;
+import faust.lhipgame.gameentities.AnimatedEntity;
+import faust.lhipgame.gameentities.Killable;
+import faust.lhipgame.gameentities.enums.Direction;
+import faust.lhipgame.gameentities.enums.GameBehavior;
+import faust.lhipgame.gameentities.impl.StrixEntity;
+import faust.lhipgame.instances.AnimatedInstance;
+import faust.lhipgame.instances.Interactable;
+import faust.lhipgame.screens.GameScreen;
+
+import java.util.Objects;
+
+/**
+ * Strix enemy instance class
+ *
+ * @author Jacopo "Faust" Buttiglieri
+ */
+public class StrixInstance extends AnimatedInstance implements Interactable, Killable {
+
+    private static final float STRIX_SPEED = 30;
+    private boolean attachedToPlayer = false;
+
+    private PlayerInstance target;
+    private Timer.Task leechLifeTimer;
+    private boolean isDead = false;
+
+    public StrixInstance(float x, float y, PlayerInstance target, AssetManager assetManager) {
+        super(new StrixEntity(assetManager));
+        currentDirection = Direction.DOWN;
+        this.startX = x;
+        this.startY = y;
+        this.target = target;
+    }
+
+    @Override
+    public void doLogic(float stateTime) {
+
+        hitBox.setTransform(body.getPosition().x, body.getPosition().y + 8, 0);
+
+        if (GameBehavior.HURT.equals(currentBehavior))
+            return;
+
+        if (!attachedToPlayer && target.getBody().getPosition().dst(getBody().getPosition()) <= LINE_OF_SIGHT) {
+            currentBehavior = GameBehavior.WALK;
+            // Normal from strix position to target
+            Vector2 direction = new Vector2(target.getBody().getPosition().x - body.getPosition().x,
+                    target.getBody().getPosition().y - body.getPosition().y).nor();
+
+            // If not already attached su player
+            currentDirection = extractDirectionFromNormal(direction);
+
+            // Move towards target
+            body.setLinearVelocity(STRIX_SPEED * direction.x, STRIX_SPEED * direction.y);
+        } else {
+
+            currentBehavior = attachedToPlayer ? GameBehavior.ATTACK : GameBehavior.IDLE;
+
+            body.setLinearVelocity(0, 0);
+        }
+    }
+
+    @Override
+    public void postHurtLogic() {
+
+        // is pushed away while flickering
+        Vector2 direction = new Vector2(target.getBody().getPosition().x - body.getPosition().x,
+                target.getBody().getPosition().y - body.getPosition().y).nor();
+
+        body.setLinearVelocity(STRIX_SPEED * 4 * -direction.x, STRIX_SPEED * 4 * -direction.y);
+        currentBehavior = GameBehavior.HURT;
+        attachedToPlayer = false;
+        // Do nothing for half second
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                currentBehavior = GameBehavior.IDLE;
+            }
+        }, 0.25f);
+    }
+
+
+    /**
+     * @return true if the damage is greater or equal than the resitance
+     */
+    @Override
+    public boolean isDying() {
+        return this.damage >= getResistance();
+    }
+
+    @Override
+    public boolean isDead() {
+        return isDead;
+    }
+
+
+    @Override
+    public void createBody(World world, float x, float y) {
+
+        Objects.requireNonNull(world);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.fixedRotation = true;
+        bodyDef.position.set(x, y);
+
+        // Define shape
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(4, 2);
+
+        // Define Fixture
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 0;
+        fixtureDef.friction = 0;
+        fixtureDef.isSensor = true;
+
+        // Associate body to world
+        body = world.createBody(bodyDef);
+        body.setUserData(this);
+        body.createFixture(fixtureDef);
+
+        shape.dispose();
+
+        // Hitbox definition
+        BodyDef hitBoxDef = new BodyDef();
+        hitBoxDef.type = BodyDef.BodyType.KinematicBody;
+        hitBoxDef.fixedRotation = true;
+        hitBoxDef.position.set(x, y);
+
+        // Define shape
+        PolygonShape hitBoxShape = new PolygonShape();
+        hitBoxShape.setAsBox(4, 6);
+
+        // Define Fixture
+        FixtureDef hitBoxFixtureDef = new FixtureDef();
+        hitBoxFixtureDef.shape = hitBoxShape;
+        hitBoxFixtureDef.density = 0;
+        hitBoxFixtureDef.friction = 0;
+        hitBoxFixtureDef.isSensor = true;
+
+        // Associate body to world
+        hitBox = world.createBody(bodyDef);
+        hitBox.setUserData(this);
+        hitBox.createFixture(hitBoxFixtureDef);
+
+        hitBoxShape.dispose();
+    }
+
+    /**
+     * Draw the Entity frames using Body position
+     *
+     * @param batch
+     * @param stateTime
+     */
+    public void draw(final SpriteBatch batch, float stateTime) {
+        Objects.requireNonNull(batch);
+
+        TextureRegion frame = ((AnimatedEntity) entity).getFrame(currentBehavior, currentDirection, stateTime);
+
+        //Draw shadow
+        batch.draw(((StrixEntity) entity).getShadowTexture(), body.getPosition().x - POSITION_OFFSET, body.getPosition().y - POSITION_Y_OFFSET);
+
+        //Draw Strix
+        if (GameBehavior.IDLE.equals(currentBehavior)) {
+            batch.draw(frame, body.getPosition().x - POSITION_OFFSET, body.getPosition().y - 8 - POSITION_Y_OFFSET);
+        } else {
+
+            // If not hurt or the flickering POI must be shown, draw the texture
+            if (!mustFlicker || !GameBehavior.HURT.equals(currentBehavior)) {
+                batch.draw(frame, body.getPosition().x - POSITION_OFFSET, body.getPosition().y - POSITION_Y_OFFSET);
+            }
+
+            // Every 1/8 seconds alternate between showing and hiding the texture to achieve flickering effect
+            if (GameBehavior.HURT.equals(currentBehavior) && TimeUtils.timeSinceNanos(startTime) > GameScreen.FLICKER_DURATION_IN_NANO / 6) {
+                mustFlicker = !mustFlicker;
+
+                // restart flickering timer
+                startTime = TimeUtils.nanoTime();
+            }
+        }
+
+    }
+
+    private void leechLife(PlayerInstance playerInstance) {
+
+        // Force cancel another one must start
+        if (Objects.nonNull(leechLifeTimer)) {
+            leechLifeTimer.cancel();
+        }
+
+        //Keep leeching
+        leechLifeTimer = Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                if (attachedToPlayer) {
+                    playerInstance.hurt(1);
+                    leechLife(playerInstance);
+                    Gdx.app.log("DEBUG", "END leech timer");
+                }
+            }
+        }, 1);
+        Gdx.app.log("DEBUG", "START leech timer");
+
+    }
+
+    public boolean isAttachedToPlayer() {
+        return attachedToPlayer;
+    }
+
+    @Override
+    public void doPlayerInteraction(PlayerInstance playerInstance) {
+        // Start to leech
+        attachedToPlayer = true;
+        leechLife(playerInstance);
+
+    }
+
+    @Override
+    public void endPlayerInteraction(PlayerInstance playerInstance) {
+        // End leech and cancel timer if present
+        attachedToPlayer = false;
+        if (Objects.nonNull(leechLifeTimer)) {
+            leechLifeTimer.cancel();
+            Gdx.app.log("DEBUG", "CANCEL leech timer");
+        }
+    }
+
+    /**
+     * Method for hurting the LivingEntity
+     *
+     * @param damageReceived to be subtracted
+     */
+    @Override
+    public void hurt(int damageReceived) {
+        if (isDying()) {
+            isDead = true;
+        } else if (!GameBehavior.HURT.equals(currentBehavior)) {
+            this.damage += Math.min(getResistance(), damageReceived);
+            Gdx.app.log("DEBUG", "Instance " + this.getClass().getSimpleName() + " total damage " + damage);
+            postHurtLogic();
+        }
+    }
+
+    @Override
+    public int getResistance() {
+        return 9;
+    }
+
+}

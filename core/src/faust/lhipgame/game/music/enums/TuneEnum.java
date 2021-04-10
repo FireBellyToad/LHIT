1c770badc845a2e8044a2dcebea984f722da9043
Faust <jacopo.buttiglieri@gmail.com>
2021-04-10 11:20:41 +0200

    ++

--- core/src/faust/lhipgame/game/music/enums/TuneEnum.java
+++ core/src/faust/lhipgame/game/music/enums/TuneEnum.java
@@ -1,2 +1,20 @@
-package faust.lhipgame.game.music;public enum TuneEnum {
+package faust.lhipgame.game.music.enums;
+
+public enum TuneEnum {
+
+    TITLE("title.ogg"),
+    AMBIENCE("ambience.ogg");
+//    DANGER("ambience"),
+//    FIGHT("ambience"),
+//    CHURCH("ambience");
+
+    private String fileName;
+
+    TuneEnum(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
 }

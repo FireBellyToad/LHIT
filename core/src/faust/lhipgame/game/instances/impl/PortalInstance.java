7d9ae880541aea0280c6f19ba7716c7480dea063
Faust <jacopo.buttiglieri@gmail.com>
2021-08-28 01:11:41 +0200

    fix on portal animation

--- core/src/faust/lhipgame/game/instances/impl/PortalInstance.java
+++ core/src/faust/lhipgame/game/instances/impl/PortalInstance.java
@@ -2,11 +2,15 @@ package faust.lhipgame.game.instances.impl;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.World;
 import faust.lhipgame.LHIPGame;
+import faust.lhipgame.game.gameentities.AnimatedEntity;
 import faust.lhipgame.game.gameentities.enums.GameBehavior;
 import faust.lhipgame.game.gameentities.impl.PortalEntity;
 import faust.lhipgame.game.instances.AnimatedInstance;
+
+import java.util.Objects;
 
 /**
  * @author Portal Instance class
@@ -35,8 +39,10 @@ public class PortalInstance extends AnimatedInstance {
     @Override
     public void draw(SpriteBatch batch, float stateTime) {
         //is fixed;
+        Objects.requireNonNull(batch);
         batch.begin();
-        batch.draw(((PortalEntity) entity).getFrame(GameBehavior.IDLE, stateTime), 80 - POSITION_OFFSET , LHIPGame.GAME_HEIGHT- POSITION_OFFSET - 108);
+        TextureRegion frame = ((AnimatedEntity) entity).getFrame(currentBehavior, mapStateTimeFromBehaviour(stateTime), true);
+        batch.draw(frame, 80 - POSITION_OFFSET , LHIPGame.GAME_HEIGHT- POSITION_OFFSET - 108);
         batch.end();
     }
 }

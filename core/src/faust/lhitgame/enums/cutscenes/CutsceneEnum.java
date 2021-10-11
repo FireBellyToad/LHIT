b9b36d87d1b992571fa503f5ada81f87dd698956
Faust <jacopo.buttiglieri@gmail.com>
2021-10-11 22:25:18 +0200

    improve ingame credits

--- core/src/faust/lhitgame/enums/cutscenes/CutsceneEnum.java
+++ core/src/faust/lhitgame/enums/cutscenes/CutsceneEnum.java
@@ -13,7 +13,7 @@ import faust.lhitgame.screens.MenuScreen;
 public enum CutsceneEnum {
     INTRO("intro", 5, GameScreen.class),
     ENDGAME("endgame", 4, EndGameScreen.class),
-    CREDITS("credits", 1, MenuScreen.class);
+    CREDITS("credits", 2, MenuScreen.class);
 
     private final String key; // string key in messages.cutscenes
     private final int stepsNumber; // number of steps of the cutscene

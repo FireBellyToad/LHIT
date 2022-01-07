c0f31a9e233d796852ca52264c6d30fdd369f1ad
Faust <jacopo.buttiglieri@gmail.com>
2022-01-07 01:03:29 +0100

    fix on html builf

--- html/src/com/faust/lhitgame/client/HtmlLauncher.java
+++ html/src/com/faust/lhitgame/client/HtmlLauncher.java
@@ -3,20 +3,23 @@ package com.faust.lhitgame.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.faust.lhitgame.LHITHGame;
+import com.faust.lhitgame.LHITGame;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
                 // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
+                GwtApplicationConfiguration configuration;
+                configuration = new GwtApplicationConfiguration(true);
+                configuration.disableAudio = true;
+                return configuration;
                 // Fixed size application:
                 //return new GwtApplicationConfiguration(480, 320);
         }
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new LHITHGame();
+                return new LHITGame();
         }
 }

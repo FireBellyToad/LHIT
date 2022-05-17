4438d732087172c5e47fc81cf4981e59bb3b25d8
Faust <jacopo.buttiglieri@gmail.com>
2022-05-17 21:55:34 +0200

    customizing loadbar

--- html/src/com/faust/lhitgame/client/HtmlLauncher.java
+++ html/src/com/faust/lhitgame/client/HtmlLauncher.java
@@ -3,8 +3,12 @@ package com.faust.lhitgame.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
 import com.faust.lhitgame.LHITGame;
 import com.faust.lhitgame.saves.impl.HtmlSaveFileManager;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -21,5 +25,18 @@ public class HtmlLauncher extends GwtApplication {
         @Override
         public ApplicationListener createApplicationListener () {
                 return new LHITGame(new HtmlSaveFileManager());
+        }
+
+        @Override
+        public Preloader.PreloaderCallback getPreloaderCallback() {
+                return createPreloaderPanel(GWT.getHostPageBaseURL() + "loading_splash.png");
+        }
+
+        @Override
+        protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+                meterPanel.setStyleName("gdx-meter");
+                meterPanel.addStyleName("nostripes");
+                meterStyle.setProperty("backgroundColor", "#000000");
+                meterStyle.setProperty("backgroundImage", "none");
         }
 }

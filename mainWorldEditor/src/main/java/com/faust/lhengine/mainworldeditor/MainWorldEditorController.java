d8564c2faecfbdeadc83b8e95ff985592f7a5523
Faust <jacopo.buttiglieri@gmail.com>
2022-08-19 11:51:22 +0200

    added core depencies

--- mainWorldEditor/src/main/java/com/faust/lhengine/mainworldeditor/MainWorldEditorController.java
+++ mainWorldEditor/src/main/java/com/faust/lhengine/mainworldeditor/MainWorldEditorController.java
@@ -1,16 +1,20 @@
 package com.faust.lhengine.mainworldeditor;
 
+import com.faust.lhengine.game.rooms.enums.RoomTypeEnum;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
+import java.util.List;
+
 public class MainWorldEditorController {
     @FXML
-    private ComboBox<String> terrainTypesCombobox;
+    private ComboBox<RoomTypeEnum> terrainTypesCombobox;
 
     @FXML
     protected void createNewMainWorld() {
         System.out.println("New mainworld");
 
+        terrainTypesCombobox.setItems(FXCollections.observableList(List.of(RoomTypeEnum.values())));
     }
 }
--- mainWorldEditor/src/main/java/module-info.java
+++ mainWorldEditor/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.faust.lhengine.mainworldeditor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires core;
 
 
     opens com.faust.lhengine.mainworldeditor to javafx.fxml;
